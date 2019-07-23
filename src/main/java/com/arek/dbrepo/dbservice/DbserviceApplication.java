173d02c6295dce5bd7b85d01f6ec11f9c2925241
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-23 12:28:22 +0200

    Add configuration to application.properties

--- src/main/java/com/arek/dbrepo/dbservice/DbserviceApplication.java
+++ src/main/java/com/arek/dbrepo/dbservice/DbserviceApplication.java
@@ -2,12 +2,14 @@ package com.arek.dbrepo.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.arek.dbrepo.dbservice.repository")
 @SpringBootApplication
 public class DbserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DbserviceApplication.class, args);
+    }
 
 }
