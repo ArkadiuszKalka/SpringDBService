4ad7e0d262832a5d03d18b476b152059024dfe83
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-23 12:19:25 +0200

    Initial commit

---
+++ src/main/java/com/arek/dbrepo/dbservice/DbserviceApplication.java
@@ -0,0 +1,13 @@
+package com.arek.dbrepo.dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbserviceApplication.class, args);
+	}
+
+}
